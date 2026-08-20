da162eea3c0478d17d5926388c510a57ccfa57a1
muzamil-hussain-2004 <muzamilburfat1@gmail.com>
2026-08-20 13:36:23 +0500

    videos 3 4 5 6

---
+++ video-2/script.js
@@ -0,0 +1,7 @@
+addEventListener('click',()=>{
+alert('Hello World')
+document.body.style.backgroundColor = 'gray'
+const p = document.createElement('p')
+p.textContent = 'new engine added'
+document.body.appendChild(p)
+})
